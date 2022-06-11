29706a0a6ae72f610f30d600d3ebae62c93c0b3b
Hiep Tran <daihieptn97@gmail.com>
2022-06-11 08:49:51 +0700

    not woking

--- App.tsx
+++ App.tsx
@@ -1,181 +1,114 @@
-import React, { useRef } from "react";
-import { Alert, Animated, ScrollView, StatusBar, Text, TouchableOpacity, View } from "react-native";
-import { img_src_bg1 } from "./src";
-import Icon from "react-native-vector-icons/Ionicons";
+import * as React from "react";
+import { Text, TouchableOpacity, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
+import Animated, { interpolate, interpolateNode,  } from "react-native-reanimated";
+import { MaterialTopTabBarProps } from "@react-navigation/material-top-tabs/lib/typescript/src/types";
 
-function App() {
+function MyTabBar({ state, descriptors, navigation, position } : MaterialTopTabBarProps) {
+  return (
+    <View style={{ flexDirection: "row", paddingTop: 20 }}>
+      {state.routes.map((route, index) => {
+        const { options } = descriptors[route.key];
+        const label =
+          options.tabBarLabel !== undefined
+            ? options.tabBarLabel
+            : options.title !== undefined
+              ? options.title
+              : route.name;
 
-  const ani = useRef(new Animated.Value(1)).current;
+        const isFocused = state.index === index;
 
+        const onPress = () => {
+          const event = navigation.emit({
+            type: "tabPress",
+            target: route.key
+          });
 
-  const runAnimation = (x: number) => {
-    Animated.timing(ani, {
-      toValue: x,
-      duration: 0,
-      useNativeDriver: false
-    }).start();
-  };
+          if (!isFocused && !event.defaultPrevented) {
+            navigation.navigate(route.name);
+          }
+        };
 
-  const scale = ani.interpolate({
-    inputRange: [-219, -1],
-    outputRange: [2.5, 1],
-    extrapolate: "clamp"
-  });
+        const onLongPress = () => {
+          navigation.emit({
+            type: "tabLongPress",
+            target: route.key
+          });
+        };
+        // // modify inputRange for custom behavior
+        // const inputRange = state.routes.map((_, i) => i);
+        //
+        // console.log(position);
+        // // console.log(inputRange);
+        //
+        //
+        // // const opacity = interpolate(position, inputRange, inputRange.map((i: number) => (i === index ? 1 : 0)));
+        // const opacity = interpolateNode(position, {
+        //   inputRange,
+        //   outputRange: inputRange.map((i) => (i === index ? 1 : 0)),
+        // });
+        // console.log("opacity", opacity);
 
-  const opaHeader = ani.interpolate({
-    inputRange: [0, 1, 20, 60],
-    outputRange: [0, 0, 0.75, 1],
-    extrapolate: "clamp"
-  });
+        const inputRange = state.routes.map((_, i) => i);
+        const opacity = Animated.interpolateNode(position, {
+          inputRange,
+          outputRange: inputRange.map((i) => (i === index ? 1 : 0)),
+        });
 
-  const opaHeaderImg = ani.interpolate({
-    inputRange: [0, 1, 30, 60],
-    outputRange: [1, 1, 0.75, 0],
-    extrapolate: "clamp"
-  });
-
-
-  const hgHeaderImg = ani.interpolate({
-    inputRange: [0, 1, 30, 60],
-    outputRange: [200, 200, 150, 1],
-    extrapolate: "clamp"
-  });
-
-
-  return <View style={{ flex: 1 }}>
-
-    {/*<View style={{ height: 200, width: "100%", position: "absolute", top: 0, left: 0, right: 0 }}>*/}
-    {/*  <Image source={img_src_bg1} style={{ width: "100%", resizeMode: "center", height: "100%" }} />*/}
-    {/*</View>*/}
-    <StatusBar backgroundColor="transparent" barStyle={"dark-content"} translucent={true} />
-
-    <Animated.View style={{
-      height: 200,
-      width: "100%",
-      overflow: "hidden",
-      position: "absolute",
-      top: 0,
-      transform: [{ scale: scale }],
-      opacity: opaHeaderImg
-    }}>
-      <Animated.Image source={img_src_bg1}
-                      style={{
-                        width: "100%",
-                        resizeMode: "stretch",
-                        height: "100%",
-                        transform: [{ scale: scale }],
-                        zIndex: 90
-
-                      }} />
-    </Animated.View>
-
-    <Animated.View
-      style={{
-        flexDirection: "row",
-        backgroundColor: "white",
-        position: "absolute",
-        top: 0,
-        left: 0,
-        width: "100%",
-        height: 80,
-        paddingTop: StatusBar.currentHeight ? StatusBar.currentHeight : 40,
-        shadowColor: "#000",
-        shadowOffset: {
-          width: 0,
-          height: 2
-        },
-        shadowOpacity: 0.25,
-        shadowRadius: 3.84,
-
-        elevation: 5,
-        justifyContent: "space-between",
-        paddingHorizontal: 14,
-        zIndex: 999,
-        opacity: 1
-      }}>
-
-      <TouchableOpacity onPress={() => Alert.alert("Hello")}>
-        <Icon name={"arrow-back-outline"} size={30} color={"#000"} />
-      </TouchableOpacity>
-      <View style={{ justifyContent: "center" }}>
-        <Text style={{ fontSize: 20, fontWeight: "bold", color: "red" }}>
-          Hiệp trần
-        </Text>
-      </View>
-      <View>
-        <Icon name={"alert-circle-outline"} size={30} color={"#000"} />
-      </View>
-    </Animated.View>
-
-
-    <ScrollView
-      scrollEventThrottle={16}
-      onScroll={(event) => {
-        console.log(event.nativeEvent.contentOffset.y);
-        runAnimation(Math.floor(event.nativeEvent.contentOffset.y));
-      }
-      }>
-
-      <Animated.View style={{ height: 200, width: "100%" }}>
-        <Animated.Image
-          source={img_src_bg1}
-          style={{
-            width: "100%",
-            resizeMode: "stretch",
-            height: "100%",
-            opacity: 0
-            // transform: [{ scale: scale }]
-
-          }} />
-      </Animated.View>
-
-
-      <ItemMenu />
-      <ItemMenu />
-
-      <Text style={{ fontSize: 20, fontWeight: "bold", color: "red" }}>
-        Hiệp trần
-      </Text>
-
-      <ItemMenu />
-      <ItemMenu />
-      <ItemMenu />
-      <ItemMenu />
-      <ItemMenu />
-      <ItemMenu />
-      <ItemMenu />
-      <ItemMenu />
-      <ItemMenu />
-      <ItemMenu />
-      <ItemMenu />
-      <ItemMenu />
-      <ItemMenu />
-      <ItemMenu />
-      <ItemMenu />
-    </ScrollView>
-  </View>;
+        return (
+          <TouchableOpacity
+            key={route.name}
+            accessibilityRole="button"
+            accessibilityState={isFocused ? { selected: true } : {}}
+            accessibilityLabel={options.tabBarAccessibilityLabel}
+            testID={options.tabBarTestID}
+            onPress={onPress}
+            onLongPress={onLongPress}
+            style={{ flex: 1 }}
+          >
+            <Animated.Text style={{ opacity }}>{label}</Animated.Text>
+          </TouchableOpacity>
+        );
+      })}
+    </View>
+  );
 }
 
-function ItemMenu() {
-  return <View style={{
-    width: "80%", alignSelf: "center", alignItems: "center",
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 2
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 3.84,
-
-    elevation: 5,
-    backgroundColor: "white",
-    padding: 10,
-    marginBottom: 14
-  }}>
-
-    <Text>Hello</Text>
-    <View style={{ backgroundColor: "green", width: "100%", height: 100 }} />
-  </View>;
+function HomeScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Home!</Text>
+    </View>
+  );
 }
 
-export default App;
+function SettingsScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Settings!</Text>
+    </View>
+  );
+}
+
+function ProfileScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Profile!</Text>
+    </View>
+  );
+}
+
+const Tab = createMaterialTopTabNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator tabBar={(props) => <MyTabBar {...props} />}>
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Settings" component={SettingsScreen} />
+        <Tab.Screen name="Profile" component={ProfileScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
